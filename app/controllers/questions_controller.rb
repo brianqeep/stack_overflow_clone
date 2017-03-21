14f8c9f7b1f3924b751d8db9fd88f388e065571e
brianqeep <brites009@gmail.com>
2017-03-21 12:50:14 +0300

    database relationship running

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,5 +6,21 @@ class QuestionsController < ApplicationController
   def show
       @question = Question.find(params[:id])
       render :show
+  end
+  def new
+      @question = Question.new
+      render :new
+    end
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to  questions_path
+    else
+      render :new
+    end
+  end
+  private
+    def question_params
+      params.require(:question).permit(:name, :question)
     end
 end
